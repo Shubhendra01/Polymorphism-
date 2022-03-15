5e29930b6a251387e50f8f07fa1d3bcf7a17f6e3
unknown <shubhhhh17@gmail.com>
2022-03-16 00:01:26 +0530

    Polymorphism Successfull

---
+++ src/Java03/Poly.java
@@ -0,0 +1,23 @@
+package Java03;
+
+public class Poly {
+
+    public int AddNumbers(int x, int y) {
+        return x + y;
+    }
+
+    public int AddNumbers(int x, int y, int z) {
+        return x + y + z;
+    }
+
+
+    public static void main(String[] args) {
+
+        Poly ob = new Poly();
+        int result1 = ob.AddNumbers(12, 60);
+        int result2 = ob.AddNumbers(12, 23, 60);
+        System.out.println(result1);
+        System.out.println(result2);
+
+    }
+}
