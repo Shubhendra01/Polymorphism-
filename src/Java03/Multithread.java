973917d5b6d6f97b9dc5e29cd1f892042f36b5af
unknown <shubhhhh17@gmail.com>
2022-03-22 14:57:24 +0530

    Multithreading Successfull with 3 classes

--- src/Java03/Multithread.java
+++ src/Java03/Multithread.java
@@ -1,18 +1,33 @@
 package Java03;
 
-class Task extends Thread{
+class Task1 extends Thread{
     @Override
     public void run(){
-        System.out.println("Thread Executed-"+ Thread.currentThread().getName());
+        System.out.println("Task1 Thread Executed-"+ Thread.currentThread().getName());
     }
 }
+class Task2 extends Thread{
+    @Override
+    public void run(){
+        System.out.println("Task2 Thread Executed-"+ Thread.currentThread().getName());
+    }
+}
+class Task3 extends Thread{
+    @Override
+    public void run(){
+        System.out.println("Task3 Thread Executed-"+ Thread.currentThread().getName());
+    }
+}
+
 public class Multithread {
     public static void main(String[] args) {
 
-       for (int i=0;i<10;i++)
-       {
-           Task t=new Task();
-           t.start();
-       }
+        Task1 t1=new Task1();
+        Task2 t2=new Task2();
+        Task3 t3=new Task3();
+        t1.start();
+        t2.start();
+        t3.start();
+
     }
 }
